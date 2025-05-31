43257cf0ee4bd57440c0ffd20c0a662bb7675cae
Austin Zhu <austinzhu666@gmail.com>
2025-05-31 22:54:57 +0900

    refactor(x): update signer test

--- packages/x/src/security/sign/signer.spec.ts
+++ packages/x/src/security/sign/signer.spec.ts
@@ -7,15 +7,20 @@ describe('TransactionIdSigner', () => {
   const homeHtml = readFileSync(path.join(import.meta.dirname, '__fixtures__', 'home.html'), 'utf-8');
   const onDemandFile = readFileSync(path.join(import.meta.dirname, '__fixtures__', 'ondemand.s.57b4929a.js'), 'utf-8');
 
+  let signer: TransactionIdSigner;
+
+  beforeEach(() => {
+    vi.spyOn(Math, 'random').mockReturnValue(0.5);
+    vi.setSystemTime(Date.UTC(2025, 0, 1));
+
+    vi.spyOn(HomeHtmlParser, 'create').mockResolvedValue(new HomeHtmlParser(homeHtml));
+    vi.spyOn(OnDemandJsParser, 'create').mockResolvedValue(new OnDemandJsParser(onDemandFile));
+
+    signer = new TransactionIdSigner();
+  });
+
   describe('sign', () => {
-    it('should generate a transaction ID', async () => {
-      vi.spyOn(Math, 'random').mockReturnValue(0.5);
-      vi.setSystemTime(Date.UTC(2025, 0, 1));
-
-      vi.spyOn(HomeHtmlParser, 'create').mockResolvedValue(new HomeHtmlParser(homeHtml));
-      vi.spyOn(OnDemandJsParser, 'create').mockResolvedValue(new OnDemandJsParser(onDemandFile));
-
-      const signer = new TransactionIdSigner();
+    it('should sign request and return transaction ID', async () => {
       const transactionId = await signer.sign('POST', '/i/api/1.1/some-endpoint.json');
 
       expect(transactionId).toMatchSnapshot();
