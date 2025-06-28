813b452fe5802ae3cb0b14237479aef7c73410fc
Austin Zhu <austinzhu666@gmail.com>
2025-06-29 00:04:25 +0900

    docs(facebook): add credential docs

--- packages/facebook/src/auth/credential.ts
+++ packages/facebook/src/auth/credential.ts
@@ -1,10 +1,30 @@
+/**
+ * Credential for accessing Facebook Pages
+ * @see {@link https://developers.facebook.com/docs/facebook-login/access-tokens#pagetokens}
+ */
 interface PageCredential {
+  /**
+   * The User ID of the Facebook user who has granted access to the Page.
+   */
   userId: string;
+  /**
+   * The Access Token of the user who has granted access to the Page.
+   */
   userAccessToken: string;
 }
 
+/**
+ * Credential for accessing on behalf of a Facebook App
+ * @see {@link https://developers.facebook.com/docs/facebook-login/access-tokens#clienttokens}
+ */
 interface ClientCredential {
+  /**
+   * The App ID of the Facebook App.
+   */
   appId: string;
+  /**
+   * The Client Access Token for the Facebook App.
+   */
   clientToken: string;
 }
 
