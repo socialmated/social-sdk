35bda7e53a0c55eaf0d4476785991784fb6ae42c
Austin Zhu <austinzhu666@gmail.com>
2025-07-01 23:47:07 +0900

    feat(facebook): implement FacebookPublicAPIClient for Graph API v23.0

---
+++ packages/facebook/src/client/api.ts
@@ -0,0 +1,23 @@
+import { type OAuthSession } from '@social-sdk/auth/session';
+import { PublicAPIClient } from '@social-sdk/client/api';
+import { type HttpClient } from '@social-sdk/client/http';
+import { createFacebookHttpClient } from './http.js';
+
+export class FacebookPublicAPIClient extends PublicAPIClient<OAuthSession> {
+  /**
+   * The HTTP client for Facebook Graph API v23.0.
+   */
+  private v23: HttpClient;
+
+  /**
+   * Creates a new instance of the Facebook API client with authenticated HTTP client.
+   *
+   * @param session - The OAuth session used for authentication with Facebook Graph API
+   */
+  constructor(session: OAuthSession) {
+    super(session);
+
+    const http = createFacebookHttpClient(session);
+    this.v23 = http.extend({ prefixUrl: 'https://graph.facebook.com/v23.0/' });
+  }
+}
